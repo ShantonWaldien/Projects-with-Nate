9ed71bf63f718785f8337932de05198719f6dae2
Jamie-leeCupido <cjamielee01@gmail.com>
2018-06-29 17:15:56 +0200

    upload attempt3

---
+++ Desktop/java/js/main.js
@@ -0,0 +1,32 @@
+// document.write('Hello darkness my old friend')
+// console.log(j);
+// console.log(x);
+// var x=0;
+
+
+// console.log(a);
+// var a=2;
+// // console.log(b);
+// // console.log(c); refer to note book on hoisting
+// let c=2;
+// console.log(c);
+//
+// // var is function scoped
+// function foo() {
+//   var i=5;
+// }
+// console.log(i);
+// // undefined
+//
+// /
+
+// var is  not block scope
+// var x=5;
+// if (true){
+//   var x=6;
+// }
+//
+// console.log(x);
+// // 6
+
+  var ptag = document.getElementById('ptag');
---
+++ Desktop/outdoor adveres/README.md
@@ -0,0 +1,1 @@
+# JamieleeCupido_OutdoorAdventures
